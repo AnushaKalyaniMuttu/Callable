d09101eda6bfd862c4c58673cd860d8fa447131a
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2023-09-20 18:52:04 +0530

    Create Callable.java

---
+++ Callable.java
@@ -0,0 +1,22 @@
+import java.util.Random; 
+import java.util.concurrent.Callable; 
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+  
+// Class
+// Implementing the Callable interface
+class CallableMessage implements Callable<String>{
+  public String call() throws Exception{
+      return "Hello World!";
+  } 
+}
+ 
+public class CallableExample{
+  static ExecutorService executor = Executors.newFixedThreadPool(2);
+    public static void main(String[] args) throws Exception{
+        CallableMessage task = new CallableMessage();
+         Future<String> message = executor.submit(task);
+         System.out.println(message.get().toString());
+    }
+}
